183f4c66efbd2d04343fb5341a5c3f92561c65df
SM_D <seanmarek@gmail.com>
2015-01-18 21:20:08 -0800

    test commit?

--- BuildContent.bat
+++ BuildContent.bat
@@ -1,4 +1,3 @@
-Echo off
 IF EXIST MageTastic\bin\Debug\Assets ( ContentCompiler.exe .\Assets .\MageTastic\bin\Debug\Assets )
 IF EXIST MageTastic\bin\Release\Assets ( ContentCompiler.exe .\Assets .\MageTastic\bin\Release\Assets )
 
