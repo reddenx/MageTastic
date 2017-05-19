356e31ac23f6ee4365ac14d0a7becae15d02ab28
sm_d3po <seanmarek@gmail.com>
2017-05-18 23:06:46 -0700

    I'm not sure what to do about physics right now... moving on

---
+++ Spellers/Physics/Body.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spellers.Physics
+{
+    class Body<T>
+    {
+        public Vector2 Position;
+        public Vector2 Velocity;
+        public Vector2 Force;
+
+        public float Rotation;
+        public float AngularVelocity;
+        public float Torque;
+
+        public float Mass;
+        //probably not going to need these until a full rigid body simulator is necessary
+        //public float InverseMass;
+        //public float Inertia;
+        //public float InverseInertia;
+
+        public float StaticFriction;
+        public float DynamicFriction;
+        public float Bounciness;
+
+        public bool IsAtRest
+        {
+            get { return Velocity.LengthSquared() < 10 && Math.Abs(AngularVelocity) < .05f; }
+        }
+
+        public T Shape;
+
+        public Body(
+            T shape,
+            float mass,
+            Vector2 position,
+            int width,
+            int height,
+            float rotation,
+            Vector2? velocity = null,
+            float angularVelocity = 0,
+            float staticFriction = .8f,
+            float dynamicFriction = .3f,
+            float bounciness = .2f)
+        {
+            this.Shape = shape;
+            this.Velocity = velocity ?? Vector2.Zero;
+            this.Force = Vector2.Zero;
+
+            this.AngularVelocity = angularVelocity;
+            this.Torque = 0;
+
+            this.Mass = mass;
+            this.InverseMass = 1f / mass;
+            this.Inertia = GetInertia(mass, width, height);
+            this.InverseInertia = 1f / Inertia;
+
+            this.StaticFriction = staticFriction;
+            this.DynamicFriction = dynamicFriction;
+            this.Bounciness = bounciness;
+        }
+
+        public void ApplyImpulse(Vector2 impulse, Vector2 contactVector)
+        {
+            Velocity += InverseMass * impulse;
+            AngularVelocity += InverseInertia * contactVector.Cross(impulse);
+        }
+
+        public void ApplyForce(Vector2 force)
+        {
+            Force += force;
+        }
+
+        private static float GetInertia(float mass, float width, float height)
+        {
+            return (mass / 12) * ((height * height) + (width * width));
+        }
+
+        public Rectangle GetBounds()
+        {
+            var collisionRadius = Shape.CollisionRadius;
+            var fudge = 5;
+            return new Rectangle(
+                (int)(Position.X - collisionRadius - fudge),
+                (int)(Position.Y - collisionRadius - fudge),
+                (int)(Shape.CollisionRadius * 2 + (fudge * 2)),
+                (int)(Shape.CollisionRadius * 2 + (fudge * 2)));
+        }
+    }
+}
---
+++ Spellers/Utilities/Vector2Extensions.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spellers
+{
+    public static class Vector2Extensions
+    {
+        public static bool IntersectFast(this Rectangle a, Rectangle b)
+        {
+            return a.Intersects(b);
+            //return (Math.Abs(a.X - b.X) < (a.Width + b.Width) / 2) && (Math.Abs(a.Y - b.Y) < (a.Height + b.Height) / 2);
+        }
+
+        public static float Cross(this Vector2 a, Vector2 b)
+        {
+            return a.X * b.Y - a.Y * b.X;
+        }
+
+        public static Vector2 Cross(this float a, Vector2 b)
+        {
+            return new Vector2(-a * b.Y, a * b.X);
+        }
+
+        public static float Dot(this Vector2 a, Vector2 b)
+        {
+            return a.X * b.X + a.Y * b.Y;
+        }
+
+        public static float Squared(this float a)
+        {
+            return a * a;
+        }
+
+        public static Vector2 Unit(this Vector2 v)
+        {
+            if (v == Vector2.Zero)
+                return Vector2.Zero;
+            return v / v.Length();
+        }
+
+        public static Vector2 RotatePoint(this Vector2 location, Vector2 pivot, float rotation)
+        {
+            var X = (float)(pivot.X + (location.X - pivot.X) * Math.Cos(rotation) - (location.Y - pivot.Y) * Math.Sin(rotation));
+            var Y = (float)(pivot.Y + (location.Y - pivot.Y) * Math.Cos(rotation) + (location.X - pivot.X) * Math.Sin(rotation));
+            return new Vector2(X, Y);
+        }
+
+        public static Vector2 ProjectOnto(this Vector2 a, Vector2 b)
+        {
+            return (a.DotProduct(b) / b.DotProduct(b)) * b;
+        }
+
+        public static float ProjectOntoScalar(this Vector2 a, Vector2 b)
+        {
+            return (a.DotProduct(b) / b.DotProduct(b)) * b.Length();
+        }
+
+        public static float CrossProduct(this Vector2 a, Vector2 b)
+        {
+            return (a.X * b.Y) - (a.Y * b.X);
+        }
+
+        public static Vector2 CrossProduct(this float s, Vector2 v)
+        {
+            return new Vector2(-s * v.Y, s * v.X);
+        }
+
+        public static Vector2 CrossProduct(this Vector2 v, float s)
+        {
+            return new Vector2(s * v.Y, -s * v.X);
+        }
+
+        public static float DotProduct(this Vector2 a, Vector2 b)
+        {
+            return (a.X * b.X) + (a.Y * b.Y);
+        }
+    }
+}
